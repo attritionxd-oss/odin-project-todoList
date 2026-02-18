ba77589f5014bd352d11c282e5cc14fc7b952bec
AtypicalLogic <archer.mansueto@gmail.com>
2026-02-18 20:24:43 +0800

    Adding favicons-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,7 +15,9 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Project: Todo List",
+      template: "./src/template.html",
     }),
+    new FaviconsWebpackPlugin("./src/logo.png"),
   ],
   output: {
     path: path.resolve(__dirname, "dist"),
