04e171e5c93364e1ae8144872c31d141f66a61ee
AtypicalLogic <archer.mansueto@gmail.com>
2026-02-18 15:29:26 +0800

    removing early stage tests

--- src/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("body").style.backgroundColor = "#111";
-
-if (process.env.NODE_ENV != "production") {
-  console.log("Looks like we are in development mode!");
-} else {
-  console.log("Looks like we are in production mode!");
-}
